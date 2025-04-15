e2c03a44b01c0b5baf05c6062e477a84def0cb75
Worston <arnob3753@gmail.com>
2025-04-16 02:49:25 +0600

    added init

--- offline_1/heuristics/__init__.py
+++ offline_1/heuristics/__init__.py
@@ -1,1 +1,5 @@
 # Allows importing all heuristics easily
+from .hamming import hamming_distance
+from .manhattan import manhattan_distance
+from .euclidean import euclidean_distance
+from .linear_conflict import linear_conflict
--- offline_1/main.py
+++ offline_1/main.py
@@ -1,3 +1,6 @@
+from heuristics import manhattan_distance, hamming_distance, euclidean_distance, linear_conflict
+
+
 # Entry point; handles input/output and user interaction
 if __name__ == "__main__":
     print("Welcome to the N-Puzzle Solver!")
